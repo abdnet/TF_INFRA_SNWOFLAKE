380a131e9f3a20d930d2b828b78554b49624e215
abdnet <idouhammou.a@outlook.fr>
2024-08-07 13:33:59 +0200

    update project

--- main.tf
+++ main.tf
@@ -1,21 +1,20 @@
 terraform {
   required_providers {
     snowflake = {
-      source  = "chanzuckerberg/snowflake"
-      version = "0.25.17"
-    }
-  }
-
-  backend "remote" {
-    organization = "data_platform"
-
-    workspaces {
-      name = "DPF_DEV"
+      source = "Snowflake-Labs/snowflake"
     }
   }
 }
 
 provider "snowflake" {
+  account                = "lw22623" # required if not using profile. Can also be set via SNOWFLAKE_ACCOUNT env var
+  username               = "TF_USR_DEV" # required if not using profile or token. Can also be set via SNOWFLAKE_USER env var
+  password               = "changeme"
+  region                 = "eu-west-3.aws"
+}
+
+provider "snowflake" {
+  profile = "securityadmin"
 }
 
 resource "snowflake_database" "demo_db" {
